338ed373c1cbed548c998547b17bbff86e76deb7
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-12-01 22:40:17 +0200

    start doing lab 38

--- store-front/src/Component/products.js
+++ store-front/src/Component/products.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { chooseList } from '../store/product-reducer';
+import * as actions from '../store/product-reducer';
 import { Box, CardMedia, Container, Grid, Card, CardContent, CardActions, Button, Typography, CardActionArea } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import {addToCart} from '../store/cart.js'
+import * as actionsCart from '../store/cart.js'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -45,13 +45,69 @@ const useStyles = makeStyles((theme) => ({
 
 const Status = props => {
   console.log('................', props);
+  useEffect(() => { props.getProduct(); }, []);
 
   const classes = useStyles();
   // console.log('proooooooooo',props);
+  const updateFunctions = item => {
+    props.addToCart(item)
+    props.decreaseInStock(item)
+    // props.updateRemoteCart(props.cartData.cartItem)
+  }
+
   return (
 
     <>
       <section className="product">
+        <ul>
+          <Box textAlign="center">
+            <Typography variant="h2" color="textPrimary">
+              {props.categoryData.activeCategory}
+            </Typography>
+
+          </Box>
+
+          {props.productData.results.map((item, idx) => {
+            if (item.category === props.categoryData.activeCategory) {
+
+              return (
+                <Container maxWidth="md" component="main" key={idx}>
+                  <Grid className={classes.grid1} container direction="raw" justify="center" alignItems="center">
+                    <Grid className={classes.grid2} container item xs={12} sm={6} lg={4} >
+
+                      <Card key={idx} className={classes.card}>
+                        <CardMedia
+                          className={classes.media}
+                          image={item.img}
+                          title={item.name}
+                          description={item.description}
+                        />
+                        <CardContent>
+                          <Typography variant="h5" color="textPrimary">
+                            {item.name}
+                          </Typography>
+                          <Typography variant="p" color="textSecondary">
+                            price : {item.price}$
+                                            <br />
+                                            inStock :  {item.inStock}
+                          </Typography>
+                        </CardContent>
+                        <CardActions>
+                          <Button key={idx} size="large" style={{ fontSize: '0.9rem' }} color="secondary" onClick={() => { item.inStock > 0 ? updateFunctions(item) : <br /> }
+                          }>Add to my cart </Button>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  </Grid>
+                </Container>
+              )
+            }
+          })}
+        </ul>
+
+      </section>
+
+      {/* <section className="product">
         <ul>
           <Box textAlign="center">
             <Typography variant="h2" color="textPrimary">
@@ -98,18 +154,26 @@ const Status = props => {
           })}
         </ul>
 
-      </section>
+      </section> */}
 
     </>
   )
 }
 
 
-const mapStateToProps = state => {
-  return state;
-};
+const mapStateToProps = state => ({
+  // return state;
+  productData: state.productData,
+  categoryData: state.categoryData,
+  cart: state.cart,
+});
 
 
-const mapDespatchToProps = { chooseList,addToCart                                                        };
+const mapDispatchToProps  = (dispatch) => ({
+  decreaseInStock: (product) => dispatch(actions.decreaseInStock(product)),
+  addToCart: (product) => dispatch(actionsCart.addToCart(product)),
+  updateRemoteCart: (product) => dispatch(actionsCart.updateRemoteCart(product)),
+  getProduct: () => dispatch(actions.getProductData())
+});
 
-export default connect(mapStateToProps, mapDespatchToProps)(Status)
+export default connect(mapStateToProps, mapDespatchToProps)(Status);
